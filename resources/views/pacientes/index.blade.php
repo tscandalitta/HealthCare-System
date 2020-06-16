543c8d665067f11d3ff6d77f55356b73c8bf524e
tscandalitta <tscandalitta@gmail.com>
2020-06-16 15:01:06 -0300

    Cursor:pointer en la tabla

--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -5,6 +5,7 @@
 @section ('custom-css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="/css/table.css">
 @endsection
 
 @section ('content')
