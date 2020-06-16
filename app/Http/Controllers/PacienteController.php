b0e8b76e39e80ae760a0e272ac74ebdeddc95ae9
tscandalitta <tscandalitta@gmail.com>
2020-06-16 10:52:15 -0300

    no message

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -10,57 +10,56 @@ class PacienteController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $pacientes = DB::table('pacientes')->get();
 
-        return \view('pacientes.index', compact('pacientes'));
+        return view('pacientes.index', compact('pacientes'));
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('pacientes.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $this->validate(request(), [
+            'dni' => 'unique:pacientes'
+        ]);
+
+        Paciente::create(request(['nombre','apellido','dni','telefono','direccion','historia_clinica']));
+
+        return redirect('/index');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Paciente  $paciente
-     * @return \Illuminate\Http\Response
      */
     public function show(Paciente $paciente)
     {
-        return view('pacientes.show');
+        return view('pacientes.show',compact('paciente'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Paciente  $paciente
-     * @return \Illuminate\Http\Response
      */
     public function edit(Paciente $paciente)
     {
-        //
+        // GET pacientes/id/edit
     }
 
     /**
@@ -68,21 +67,19 @@ class PacienteController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Paciente  $paciente
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Paciente $paciente)
     {
-        //
+        // PATCH /pacientes/id
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Paciente  $paciente
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Paciente $paciente)
     {
-        //
+        // DELETE /pacientes/id
     }
 }
--- public/js/table.js
+++ public/js/table.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+    $('#tabla-pacientes tbody tr').click(function() {
+        var paciente = $(this).find("#td-dni").html();    
+        alert(paciente);    
+    });
+    
     $('#tabla-pacientes').DataTable();
 });
