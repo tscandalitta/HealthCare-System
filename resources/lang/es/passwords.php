b5622c015d0c2cb7e79476123d7343f6dd83ddaa
tscandalitta <tscandalitta@gmail.com>
2020-06-16 15:00:45 -0300

    Traduccion de mensajes de error

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Su contraseña ha sido restaurada!',
+    'sent' => 'Hemos enviado un email con el link de restauración de contraseña!',
+    'throttled' => 'Por favor, espere antes de reintentar.',
+    'token' => 'El token de restauración de contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con dicho email.",
+
+];
