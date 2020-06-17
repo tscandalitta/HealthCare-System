c35daac7e4479ba4c8d98653bc1d054de4d1f799
tscandalitta <tscandalitta@gmail.com>
2020-06-17 16:40:18 -0300

    Separo navbar en otro archivo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,71 +15,21 @@
 
     @yield('custom-css')
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-            <a class="navbar-brand" href="/pacientes/index">
-            <img src="/assets/logo-fondo-blanco.png" alt="" style="max-width: 1.75em; height: auto; m-auto">
-        </a>
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrase') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Cerrar sesión') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('layouts.navbar1')
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
 
-<!-- Scripts -->
+    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     {{-- <script src="/resources/js/app.js"></script> --}}
-    
+
     @yield('scripts')
 </body>
+
 </html>
--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -3,40 +3,63 @@
 @section ('title', 'Inicio')
 
 @section ('custom-css')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="/css/table.css">
 @endsection
 
 @section ('content')
-<div class="container">
+<div class="container-fluid">
 
+    <div class="row">
+        <div class="col-lg-5">
+            <div class="table-responsive">
+                <table id="tabla-pacientes" class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">DNI</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($pacientes as $paciente)
+                        <tr onclick="">
+                            <td>{{ $paciente->apellido }}</td>
+                            <td>{{ $paciente->nombre }}</td>
+                            <td id="td-dni">{{ $paciente->dni }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-<div class="row">
-    <div class="col">
-        <div class="table-responsive">
-            <table id="tabla-pacientes" class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Apellido</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">DNI</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($pacientes as $paciente)
-                    <tr>
-                        <td>{{ $paciente->apellido }}</td>
-                        <td>{{ $paciente->nombre }}</td>
-                        <td id="td-dni">{{ $paciente->dni }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="col-lg-7">
+            <div class="card" id="card-pacientes">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col">
+
+                            <h4 id="field-nombre"></h4>
+                            <label for="field-dni">DNI: </label>
+                            <label id="field-dni"></label>
+
+                        </div>
+                        <div class="col">
+                            <h6 id="field-telefono"></h6>
+                            <h6 id="field-direccion"></h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <label for="field-historiaClinica">Historia clínica</label>
+                    <textarea class="form-control mb-3" id="field-historiaClinica" rows="8"></textarea>
+                    <a href="#" class="btn btn-primary">Guardar HC</a>
+
+                </div>
+            </div>
         </div>
     </div>
-    <div class="col"></div>
-</div>
 </div>
 @endsection
 
