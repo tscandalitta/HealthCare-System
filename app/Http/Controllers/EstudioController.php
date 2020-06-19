e56bde1a9e1e4f14c91116e5899684ad6200dfda
tscandalitta <tscandalitta@gmail.com>
2020-06-19 17:56:56 -0300

    Creo entidad estudio

---
+++ app/Http/Controllers/EstudioController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Estudio;
+use Illuminate\Http\Request;
+
+class EstudioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Estudio  $estudio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Estudio $estudio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Estudio  $estudio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Estudio $estudio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Estudio  $estudio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Estudio $estudio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Estudio  $estudio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Estudio $estudio)
+    {
+        //
+    }
+}
---
+++ database/factories/EstudioFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Estudio;
+use Faker\Generator as Faker;
+
+$factory->define(Estudio::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
