d17fd65079f8acba7487d172699d748e9c839614
tscandalitta <tscandalitta@gmail.com>
2020-06-23 10:32:18 -0300

    Agrupo routes segun permisos

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'HomeController@index');
+Route::group(['middleware' => 'can:update pacientes'], function () {
+    Route::get('/pacientes/{paciente}/edit', 'PacienteController@edit');
+    Route::patch('/pacientes/{paciente}','PacienteController@update');
+    Route::patch('/pacientes/{paciente}/hc','PacienteController@updateHC');
+});
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'can:create pacientes'], function () { 
+    Route::get('/pacientes/create', 'PacienteController@create');
+    Route::post('/pacientes', 'PacienteController@store');
+});
 
-Route::get('/index', 'PacienteController@index')->name('home');
+Route::group(['middleware' => 'can:view pacientes'], function () { 
+    Route::get('/index', 'PacienteController@index')->name('home');
+    Route::get('/pacientes/{dni}', 'PacienteController@show');
+});
 
-Route::get('/pacientes/create', 'PacienteController@create');
+Route::group(['middleware' => 'can:delete pacientes'], function () { 
+    Route::delete('/pacientes/{paciente}','PacienteController@destroy');
+});
 
-Route::get('/pacientes/{dni}', 'PacienteController@show');
 
-Route::get('/pacientes/{paciente}/edit', 'PacienteController@edit');
-
-Route::post('/pacientes', 'PacienteController@store');
-
-Route::patch('/pacientes/{paciente}','PacienteController@update');
-
-Route::patch('/pacientes/{paciente}/hc','PacienteController@updateHC');
-
-Route::delete('/pacientes/{paciente}','PacienteController@destroy');
+Route::get('', function() {
+    return view('home');
+});
 
 Auth::routes();
 
 Route::get('/prueba', function() {
     return view('layouts.prueba');
+    return view('home');
 });
