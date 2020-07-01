e500190f3d4d4a4e89011aef50fac78ae5a1c787
tscandalitta <tscandalitta@gmail.com>
2020-07-01 17:57:19 -0300

    oculto token

--- app/Paciente.php
+++ app/Paciente.php
@@ -4,6 +4,8 @@ namespace App;
 
 class Paciente extends Model 
 {
+    protected $hidden = ['id','created_at','updated_at','token'];
+
     public function obraSocial()
     {
        return $this->belongsTo(ObraSocial::class);
