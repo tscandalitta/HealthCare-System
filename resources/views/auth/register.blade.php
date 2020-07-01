e4a760a43fb5f027453f202603219525aa580040
tscandalitta <tscandalitta@gmail.com>
2020-07-01 15:51:19 -0300

    solicito token de paciente en registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="token" class="col-md-4 col-form-label text-md-right">{{ __('Token') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="token" type="text" class="form-control @error('token') is-invalid @enderror" name="token" value="{{ old('token') }}" required>
+
+                                @error('token')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
